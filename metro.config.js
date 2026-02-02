b861fff87e139bb98fe57dfbc3be4a443750cb84
GustosRepo <agy760mac@gmail.com>
2026-02-01 18:39:48 -0800

    Fix: Use @expo/metro-config directly for EAS builds

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,9 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+
+// Required for bare React Native + EAS
+config.resolver.sourceExts = ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs'];
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
 
 module.exports = config;
